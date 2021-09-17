a4449f04c1b018bd37956a4356d70d36e7d3573f
dshaw <deke@mytechies.com>
2021-09-17 18:18:18 -0500

    Initial commit

---
+++ x/qbitgateway/genesis.go
@@ -0,0 +1,26 @@
+package qbitgateway
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/deke-create/qbit-gateway/x/qbitgateway/keeper"
+	"github.com/deke-create/qbit-gateway/x/qbitgateway/types"
+)
+
+// InitGenesis initializes the capability module's state from a provided genesis
+// state.
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+	// this line is used by starport scaffolding # genesis/module/init
+
+	// this line is used by starport scaffolding # ibc/genesis/init
+}
+
+// ExportGenesis returns the capability module's exported genesis.
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+	genesis := types.DefaultGenesis()
+
+	// this line is used by starport scaffolding # genesis/module/export
+
+	// this line is used by starport scaffolding # ibc/genesis/export
+
+	return genesis
+}
